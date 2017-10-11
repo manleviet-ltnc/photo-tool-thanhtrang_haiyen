d0b5c3f4d777c2b000cfb0df25d722358c15485e
ThanhhTrangg <>
2017-10-12 00:00:04 +0700

    Photo tool -1

---
+++ MyPhotos/MyPhotos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyPhotos")]
+[assembly: AssemblyDescription("Sample Application for Windows Forms in Action")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Manning  Publication Co.")]
+[assembly: AssemblyProduct("MyPhotos")]
+[assembly: AssemblyCopyright("Copyright ©  Manning  Publication")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("60ca43d1-1c2f-4222-882c-439e4b7b8c62")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.2")]
+[assembly: AssemblyFileVersion("2.2")]
