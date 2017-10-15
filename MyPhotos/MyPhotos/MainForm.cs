8b6b912b8c49e2c1c368c35826cad93082fe1735
Nguyen Ngoc Hai Yen <32034503+yen2909@users.noreply.github.com>
2017-10-16 02:45:11 +0700

    ep2

--- MyPhotos/MyPhotos/MainForm.cs
+++ MyPhotos/MyPhotos/MainForm.cs
@@ -7,21 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Manning.MyPhotoAlbum;
 
 namespace MyPhotos
 {
     public partial class MainForm : Form
     {
+        private AlbumManager _manager;
+        private AlbumManager Manager
+        {
+            get
+            {
+                return _manager;
+            }
+            set
+            {
+                _manager = value;
+            }
+        }
+
         public MainForm()
         {
             InitializeComponent();
+            NewAlbum();
+        }
+
+        private void NewAlbum()
+        {
+            // TODO: clean up, save existing album
+            Manager = new AlbumManager();
+            DisplayAlbum();
+        }
+
+        private void DisplayAlbum()
+        {
+            pbxPhoto.Image = Manager.CurrentImage;
             SetTitleBar();
             SetStatusStrip(null);
         }
+
         private void SetTitleBar()
         {
             Version ver = new Version(Application.ProductVersion);
-            Text = String.Format("MyPhotos {0:0}.{1:0}", ver.Major, ver.Minor);
+            string name = Manager.FullName;
+            Text = String.Format("{2} - MyPhotos {0:0}.{1:0}",
+                                 ver.Major, ver.Minor,
+                                 string.IsNullOrEmpty(name) ? "Untitled" : name);
            
         }
 
@@ -57,6 +88,7 @@ namespace MyPhotos
         {
             ProcessImageClick(e);
         }
+
         private void ProcessImageClick(ToolStripItemClickedEventArgs e)
         {
             ToolStripItem item = e.ClickedItem;
@@ -71,6 +103,7 @@ namespace MyPhotos
         {
             ProcessImageOpening(sender as ToolStripDropDownItem);
         }
+
         private void ProcessImageOpening(ToolStripDropDownItem parent)
         {
             if (parent != null)
@@ -83,14 +116,18 @@ namespace MyPhotos
                 }
             }
         }
+
         private void SetStatusStrip(string path)
         {
             if (pbxPhoto.Image!= null)
             {
-                sttInfo.Text = path;
-                sttImageSize.Text = string.Format("{0:#} x  {1: #} ", pbxPhoto.Image.Width, pbxPhoto.Image.Height);
-                //sttAlbumPos is set in ch.6
-
+                sttInfo.Text = Manager.Current.Caption;
+                sttImageSize.Text = string.Format("{0:#}x{1: #}", 
+                                                  pbxPhoto.Image.Width, 
+                                                  pbxPhoto.Image.Height);
+                sttAlbumPos.Text = String.Format(" {0:0}/{1:0} ",
+                                                 Manager.Index + 1,
+                                                 Manager.Album.Count);
             }
             else
             {
@@ -98,6 +135,142 @@ namespace MyPhotos
                 sttImageSize.Text = null;
                 sttAlbumPos.Text = null;
             }
+        }
+
+        private void mnuFileNew_Click(object sender, EventArgs e)
+        {
+            NewAlbum();
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Album";
+            dlg.Filter = "Album files (*.abm)|*.abm" + "| All files (*.*)|*.*";
+            dlg.InitialDirectory = AlbumManager.DefaultPath;
+            dlg.RestoreDirectory = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // TODO: save many existing album
+
+                // Open the new album
+                // TODO: handle invalid album file
+                Manager = new AlbumManager(dlg.FileName);
+                DisplayAlbum();
+            }
+            dlg.Dispose();
+        }
+
+        private void SaveAlbum(string name)
+        {
+            Manager.Save(name, true);
+        }
+
+        private void SaveAlbum()
+        {
+            if (string.IsNullOrEmpty(Manager.FullName))
+                SaveAsAlbum();
+            else
+            {
+                // Save the album under the existing name 
+                SaveAlbum(Manager.FullName);
+            }
+        }
+
+        private void SaveAsAlbum()
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Title = "Save Album";
+            dlg.DefaultExt = "abm";
+            dlg.Filter = "Album files (*.abm)|*.abm" + "| All files (*.*)|*.*";
+            dlg.InitialDirectory = AlbumManager.DefaultPath;
+            dlg.RestoreDirectory = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                SaveAlbum(dlg.FileName);
+                // Update title bar to include new name
+                SetTitleBar();
+            }
+            dlg.Dispose();
+        }
+
+        private void mnuFileSave_Click(object sender, EventArgs e)
+        {
+            SaveAlbum();
+        }
+
+        private void mnuFileSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveAsAlbum();
+        }
+
+        private void mnuEditAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Title = "Add Photos";
+            dlg.Multiselect = true;
+            dlg.Filter = "Image Files (JPEG, GIF, BMP, etc.)|*.jpg;*.jpeg;*.gif;*.bmp;*.tiff;*.png|" +
+                         "JPEG files (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                         "GIF files (*.gif)|*.gif|" +
+                         "BMP files (*.bmp)|*.bmp|" +
+                         "TIFF files (*.tif;*.tiff)|*.tif;*.tiff|" +
+                         "PNG files (*.png)|*.png|" +
+                         "ALL files (*.*)|*.*";
+            dlg.InitialDirectory = Environment.CurrentDirectory;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = dlg.FileNames;
+
+                int index = 0;
+                foreach (string s in files)
+                {
+                    Photograph photo = new Photograph(s);
+
+                    index = Manager.Album.IndexOf(photo);
+                    if (index < 0)
+                        Manager.Album.Add(photo);
+                    else
+                        photo.Dispose();
+                }
+                Manager.Index = Manager.Album.Count - 1; 
+            }
+            dlg.Dispose();
+            DisplayAlbum();
+        }
+
+        private void mnuEditRemove_Click(object sender, EventArgs e)
+        {
+            if (Manager.Album.Count > 0)
+            {
+                Manager.Album.RemoveAt(Manager.Index);
+                DisplayAlbum();
+            }
+        }
+
+        private void mnuNext_Click(object sender, EventArgs e)
+        {
+            if (Manager.Index < Manager.Album.Count - 1)
+            {
+                Manager.Index++;
+                DisplayAlbum();
+            }
+        }
+
+        private void mnuPrevious_Click(object sender, EventArgs e)
+        {
+            if (Manager.Index > 0)
+            {
+                Manager.Index--;
+                DisplayAlbum();
+            }
+        }
+
+        private void ctxMenuPhoto_Opening(object sender, CancelEventArgs e)
+        {
+            mnuNext.Enabled = (Manager.Index < Manager.Album.Count - 1);
+            mnuPrevious.Enabled = (Manager.Index > 0);
         }
     }
 }
